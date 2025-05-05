7a6e451374f9bc87675e341ad030b79ecedb30e7
Max Stewie <94962460+kunhtml@users.noreply.github.com>
2025-05-05 14:30:55 +0700

    Update background.js

--- background.js
+++ background.js
@@ -4,6 +4,8 @@ chrome.runtime.onInstalled.addListener(() => {
     enabled: true,
     subscribeEnabled: true,
     waitTime: 5, // Default wait time in seconds
+    speedControlEnabled: true, // Enable YouTube speed control buttons by default
+    universalSpeedControlEnabled: true, // Enable universal speed control buttons by default
   });
 });
 
@@ -11,12 +13,26 @@ chrome.runtime.onInstalled.addListener(() => {
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   if (message.action === "getStatus") {
     chrome.storage.sync.get(
-      ["enabled", "subscribeEnabled", "waitTime"],
+      [
+        "enabled",
+        "subscribeEnabled",
+        "waitTime",
+        "speedControlEnabled",
+        "universalSpeedControlEnabled",
+      ],
       (data) => {
         sendResponse({
           enabled: data.enabled,
           subscribeEnabled: data.subscribeEnabled,
           waitTime: data.waitTime || 5, // Default to 5 seconds if not set
+          speedControlEnabled:
+            data.speedControlEnabled !== undefined
+              ? data.speedControlEnabled
+              : true, // Default to true if not set
+          universalSpeedControlEnabled:
+            data.universalSpeedControlEnabled !== undefined
+              ? data.universalSpeedControlEnabled
+              : true, // Default to true if not set
         });
       }
     );
